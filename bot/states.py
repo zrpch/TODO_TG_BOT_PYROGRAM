fe2e6973cc84f50dcdffdb6eb522f1df9f924717
zrpch <zarapchiev@gmail.com>
2025-02-04 13:09:33 +0300

    Add States and .env.template

---
+++ bot/states.py
@@ -0,0 +1,20 @@
+class Keys:
+    STATE = "state"
+    NAME = "name"
+    EDITED_TASK_ID = "edited_task_id"
+
+    TASK_TITLE = "title"
+    TASK_DESCRIPTION = "description"
+
+
+class States:
+    ENTER_NAME = "enter_name"
+    ENTER_USERNAME = "enter_username"
+
+    ENTER_TASK_TITLE = "enter_task_title"
+    ENTER_TASK_DESCRIPTION = "enter_task_description"
+
+    ENTER_TASK_NUMBER = "enter_task_number"
+
+    EDIT_TASK_TITLE = "edit_task_title"
+    EDIT_TASK_DESCRIPTION = "edit_task_description"
